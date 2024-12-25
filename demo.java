527fd5fe923bab4566f7c1d04e18048a4f7712b1
Chandan D S <121010385+Chandands1@users.noreply.github.com>
2024-12-25 19:53:41 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class A{
   public static void main(String[] args){
     System.out.println("Hey Welcome to git");
     System.out.println("Second line added from the git");
+    System.out.println("Added the third line");
   }
 }
